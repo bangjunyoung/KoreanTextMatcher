1d8803daed5a0d83ed3fb909132f346bc84ea8a4
Bang Jun-young <junyoung@mogua.com>
2014-06-24 23:00:48 +0900

    누락된 private 키워드 추가

--- src/com/mogua/localization/KoreanChar.java
+++ src/com/mogua/localization/KoreanChar.java
@@ -36,14 +36,14 @@ package com.mogua.localization;
  */
 public final class KoreanChar {
 
-    static final int CHOSEONG_COUNT = 19;
-    static final int JUNGSEONG_COUNT = 21;
-    static final int JONGSEONG_COUNT = 28;
-    static final int HANGUL_SYLLABLE_COUNT = CHOSEONG_COUNT * JUNGSEONG_COUNT * JONGSEONG_COUNT;
-    static final int HANGUL_SYLLABLES_BASE = 0xAC00;
-    static final int HANGUL_SYLLABLES_END = HANGUL_SYLLABLES_BASE + HANGUL_SYLLABLE_COUNT;
+    private static final int CHOSEONG_COUNT = 19;
+    private static final int JUNGSEONG_COUNT = 21;
+    private static final int JONGSEONG_COUNT = 28;
+    private static final int HANGUL_SYLLABLE_COUNT = CHOSEONG_COUNT * JUNGSEONG_COUNT * JONGSEONG_COUNT;
+    private static final int HANGUL_SYLLABLES_BASE = 0xAC00;
+    private static final int HANGUL_SYLLABLES_END = HANGUL_SYLLABLES_BASE + HANGUL_SYLLABLE_COUNT;
 
-    static final int[] COMPATIBILITY_CHOSEONG = new int[] {
+    private static final int[] COMPATIBILITY_CHOSEONG = new int[] {
         0x3131, 0x3132, 0x3134, 0x3137, 0x3138, 0x3139, 0x3141, 0x3142, 0x3143, 0x3145,
         0x3146, 0x3147, 0x3148, 0x3149, 0x314A, 0x314B, 0x314C, 0x314D, 0x314E
     };
