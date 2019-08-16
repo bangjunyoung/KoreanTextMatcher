d4e645da20a7e99c5fb9631408a224f0d75a32dc
Bang Jun-young <bang.junyoung@gmail.com>
2019-08-16 22:48:21 +0900

    사용하지 않는 import 삭제

--- src/test/java/io/github/bangjunyoung/KoreanTextMatcher_matchesTest.java
+++ src/test/java/io/github/bangjunyoung/KoreanTextMatcher_matchesTest.java
@@ -32,7 +32,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
